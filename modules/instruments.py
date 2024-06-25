c794d6b8ff1a2c1b1b8bec3d10e8f7246280b716
John Larson <jl@buildmidwestern.com>
2024-06-25 17:52:00 -0500

    fix: fixed instruments

--- modules/instruments.py
+++ modules/instruments.py
@@ -1,5 +1,5 @@
 displayed_instruments = ["Bagpipes", "Drum", "Dulcimer", "Flute", "Lute", "Lyre", "Horn", "Pan Flute", "Shawn", "Viol"]
-instruments = ["bagpipes", "drum", "dulcimer", "flute", "lute", "lyre", "horn", "pan flute", "shawm", "viol"]
+instruments = ["bagpipes", "drum", "dulcimer", "flute", "lute", "lyre", "horn", "pan flute", "shawn", "viol"]
 
 def display_instruments():
     for instrument in displayed_instruments:
