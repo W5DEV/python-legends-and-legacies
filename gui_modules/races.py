623d22cb2c0f558936b69b29845e0af7c6ee35f2
John Larson <jl@buildmidwestern.com>
2024-06-30 23:34:49 -0500

    feat: implemented elf race code

--- gui_modules/races.py
+++ gui_modules/races.py
@@ -120,7 +120,6 @@ def define_gender():
 
         pygame.display.update()
 
-
 def define_hill_dwarf():
     name = ""
     gender = ""
@@ -242,9 +241,193 @@ def get_dwarf_tool_proficiency():
 
         pygame.display.update()
 
+def define_high_elf():
+    name = ""
+    gender = ""
+    race = "Elf"
+    subrace = "High Elf"
+    size = "Medium"
+    speed = 30
+    languages = ["Common", "Elvish"]
+    ability_score_increase = [0, 0, 2, 1, 0, 0]
+    traits = ["Darkvision", "Keen Senses", "Fey Ancestry", "Trance", "Elf Weapon Training", "Cantrip", "Extra Language"]
+    proficiencies = ["Longsword", "Shortsword", "Longbow", "Shortbow"]
+
+    gender = define_gender()
+    name = define_elf_name(gender)
+    language = choose_languages()
+    languages.append(language)
+
+    high_elf = Race(name, gender, race, subrace, size , speed, languages, ability_score_increase, traits, proficiencies)
+    return high_elf
+
+def define_wood_elf():
+    name = ""
+    gender = ""
+    race = "Elf"
+    subrace = "Wood Elf"
+    size = "Medium"
+    speed = 35
+    languages = ["Common", "Elvish"]
+    ability_score_increase = [0, 0, 2, 0, 1, 0]
+    traits = ["Darkvision", "Keen Senses", "Fey Ancestry", "Trance", "Elf Weapon Training", "Fleet of Foot", "Mast of the Wild"]
+    proficiencies = ["Longsword", "Shortsword", "Longbow", "Shortbow"]
+
+    gender = define_gender()
+    name = define_elf_name(gender)
+
+    wood_elf = Race(name, gender, race, subrace, size , speed, languages, ability_score_increase, traits, proficiencies)
+    return wood_elf
+
+def define_elf_name(gender):
+    button_texts = ["Click to Continue..."]
+    button_rects = buttons.create_button_rects(SCREEN_WIDTH, SCREEN_HEIGHT, len(button_texts))
     
+    elf_child_names = ["Ara", "Bryn", "Del", "Eryn", "Faen", "Innil", "Lael", "Mella", "Naill", "Naeris", "Phann", "Rael", "Rinn", "Sai", "Syllin", "Thia", "Vall"]
+    elf_male_names = ["Adran", "Aelar", "Aramil", "Arannis", "Aust", "Beiro", "Berrian", "Carric", "Enialis", "Erdan", "Erevan", "Galinndan", "Hadarai", "Heian", "Himo", "Immeral", "Ivellios", "Laucian", "Mindartis", "Paelias", "Peren", "Quarion", "Riardon", "Rolen", "Soveliss", "Thamior", "Tharivol", "Theren", "Varis"]
+    elf_female_names = ["Adrie", "Althaea", "Anastrianna", "Andraste", "Antinua", "Bethrynna", "Birel", "Caelynn", "Drusilia", "Enna", "Felosial", "Ielenia", "Jelenneth", "Keyleth", "Leshanna", "Lia", "Meriele", "Mialee", "Naivara", "Quelenna", "Quillathe", "Sariel", "Shanairra", "Shava", "Silaqui", "Theirastra", "Thia", "Vadania", "Valanthe", "Xanaphia"]
+    elf_family_names = ["Amakiir", "Amastacia", "Galanodel", "Holimion", "Ilphelkiir", "Liadon", "Meliamne", "Nai'lo", "Siannodel", "Xiloscient"]
+    
+    first_names = []
+    if gender == "Male":
+        first_names = elf_male_names
+    if gender == "Female":
+        first_names = elf_female_names
 
+    family_names = elf_family_names
 
+    family_name = random.choice(family_names)
 
+    text_position = (MARGIN, MARGIN)
+    
+    animate_flag = True  # Flag to control text animation
 
+    if is_elf_child():
+        first_name = random.choice(elf_child_names)
+    else:
+        first_name = choose_elf_first_name(first_names)
 
+    name = first_name + " " + family_name
+    
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for i, rect in enumerate(button_rects):
+                    if rect.collidepoint(mouse_pos):
+                        return name
+                    
+        screen.fill(BG_COLOR)
+
+        if animate_flag:
+            text_display.animate_text(screen, f'Your name is {first_name} of clan {family_name}', text_position, TEXT_AREA_WIDTH)
+            animate_flag = False
+        else:
+            text_display.draw_text(screen, f'Your name is {first_name} of clan {family_name}', text_position, TEXT_AREA_WIDTH)
+
+        buttons.draw_buttons(screen, button_texts, button_rects, mouse_pos)
+
+        pygame.display.update()
+
+def choose_elf_first_name(first_names):
+    button_texts = first_names
+    button_rects = buttons.create_button_rects(SCREEN_WIDTH, SCREEN_HEIGHT, len(button_texts))
+    text_position = (MARGIN, MARGIN)
+    
+    animate_flag = True  # Flag to control text animation
+
+    first_name = None
+
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for i, rect in enumerate(button_rects):
+                    if rect.collidepoint(mouse_pos):
+                        first_name = button_texts[i]
+                        return first_name
+                    
+        screen.fill(BG_COLOR)
+
+        if animate_flag:
+            text_display.animate_text(screen, 'Choose your elf\'s first name!', text_position, TEXT_AREA_WIDTH)
+            animate_flag = False
+        else:
+            text_display.draw_text(screen, 'Choose your elf\'s first name!', text_position, TEXT_AREA_WIDTH)
+
+        buttons.draw_buttons(screen, button_texts, button_rects, mouse_pos)
+
+        pygame.display.update()
+
+def is_elf_child():
+    button_texts = ["Yes", "No"]
+    button_rects = buttons.create_button_rects(SCREEN_WIDTH, SCREEN_HEIGHT, len(button_texts))
+    text_position = (MARGIN, MARGIN)
+    
+    animate_flag = True  # Flag to control text animation
+
+    is_child = None
+
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for i, rect in enumerate(button_rects):
+                    if rect.collidepoint(mouse_pos):
+                        if button_texts[i] == "Yes":
+                            is_child = True
+                        else:
+                            is_child = False
+                        return is_child
+                    
+        screen.fill(BG_COLOR)
+
+        if animate_flag:
+            text_display.animate_text(screen, 'Is your elf a child?', text_position, TEXT_AREA_WIDTH)
+            animate_flag = False
+        else:
+            text_display.draw_text(screen, 'Is your elf a child?', text_position, TEXT_AREA_WIDTH)
+
+        buttons.draw_buttons(screen, button_texts, button_rects, mouse_pos)
+
+        pygame.display.update()
+
+def choose_languages():
+    languages = ["Arakora", "Abyssal/Infernal", "Aquan", "Auran", "Celestial", "Deep Speech", "Draconic", "Druidic", "Dwarvish", "Giant/Jotun", "Gith", "Gnoll", "Gnomish", "Goblin", "Halfling", "Orcish", "Primordial", "Sylvan", "Terran", "Undercommon"]
+    button_texts = languages
+    button_rects = buttons.create_button_rects(SCREEN_WIDTH, SCREEN_HEIGHT, len(button_texts))
+    text_position = (MARGIN, MARGIN)
+
+    animate_flag = True
+
+    while True:
+        mouse_pos = pygame.mouse.get_pos()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for i, rect in enumerate(button_rects):
+                    if rect.collidepoint(mouse_pos):
+                        return button_texts[i]
+
+        screen.fill(BG_COLOR)
+
+        if animate_flag:
+            text_display.animate_text(screen, 'Choose an additional language to be proficient in...', text_position, TEXT_AREA_WIDTH)
+            animate_flag = False
+        else:
+            text_display.draw_text(screen, 'Choose an additional language to be proficient in...', text_position, TEXT_AREA_WIDTH)
+
+        buttons.draw_buttons(screen, button_texts, button_rects, mouse_pos)
+
+        pygame.display.update()
